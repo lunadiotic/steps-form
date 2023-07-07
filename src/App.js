74622e9614a55b9497789428a5187a7c20b64928
aimeliala <idstack@outlook.co.id>
2023-07-07 11:23:24 +0700

    isi dari usestate dan jangan mengubah nilainya langsung

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import { useState } from "react";
 
 function App() {
+  // const arr = useState({ name: "aim" });
+
+  // console.log(arr);
+
   // let step = 1;
   const [step, setStep] = useState(1);
+  const [test, setTest] = useState({ name: "aim" });
+
   function handleNext() {
     // step += 1;
     if (step < 3) setStep(step + 1);
     console.log(step);
+    // bad practice
+    setTest({ name: "luna" });
     // console.log(step);
   }
 
@@ -24,7 +32,7 @@ function App() {
         <div className={step >= 2 ? "active" : ""}>2</div>
         <div className={step >= 3 ? "active" : ""}>3</div>
       </div>
-      <p className="message">Step: Dream</p>
+      <p className="message">Step: Dream {test.name}</p>
       <div className="buttons">
         <button
           style={{ backgroundColor: "#526D82", color: "#fff" }}
